233a67c31ab837ff69c046e81ec3bc921b31d941
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-03-01 21:00:12 +0000

    Add the dtool requirement to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(name="datademo",
       author="Tjelvar Olsson",
       author_email="tjelvar.olsson@jic.ac.uk",
       url=url,
-      install_requires=[],
+      install_requires=["dtool", ],
       download_url="{}/tarball/{}".format(url, version),
       license="MIT")

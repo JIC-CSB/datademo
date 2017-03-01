b534db1119a7592bd9ab0d812551c246f4e04e7a
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-03-01 22:04:43 +0000

    Add datademo dataset summary CLI

--- datademo/cli.py
+++ datademo/cli.py
@@ -28,3 +28,15 @@ def dataset():
 def identifiers(path):
     dataset = dtool.DataSet.from_path(path)
     click.secho(" ".join(dataset.identifiers))
+
+
+@dataset.command()
+@dataset_path_option
+def summary(path):
+    dataset = dtool.DataSet.from_path(path)
+    file_list = dataset.manifest["file_list"]
+    total_size = sum([f["size"] for f in file_list])
+    click.secho("Name: {}".format(dataset.name))
+    click.secho("Creator: {}".format(dataset.creator_username))
+    click.secho("Number of files: {}".format(len(file_list)))
+    click.secho("Total size: {}".format(total_size))
--- tests/test_datademo_cli.py
+++ tests/test_datademo_cli.py
@@ -15,3 +15,19 @@ def test_dataset_identifiers(dataset_fixture):  # NOQA
     cmd = ["datademo", "dataset", "identifiers", dataset_fixture]
     identifiers = subprocess.check_output(cmd).decode("utf-8")
     assert len(identifiers.split()) == 2
+
+
+def test_dataset_summary(dataset_fixture):  # NOQA
+    cmd = ["datademo", "dataset", "summary", dataset_fixture]
+    summary = subprocess.check_output(cmd).decode("utf-8")
+    summary_lines = summary.split("\n")
+    expected_lines = [
+        "Name: test",
+        "Creator:",
+        "Number of files: 2",
+        "Total size: 10",
+        "",
+    ]
+    assert len(summary_lines) == len(expected_lines)
+    for a, e in zip(summary_lines, expected_lines):
+        assert a.startswith(e)
